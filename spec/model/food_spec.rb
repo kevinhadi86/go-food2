00283b747906350fef1f771b6763366f13337233
kevinhadi86 <kevinhadinata86@gmail.com>
2018-08-07 10:50:27 +0000

    add food_helper

---
+++ spec/model/food_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+describe Food do
+    it "is valid with a name and description" do
+        food = Food.new(
+            name: "Nasi Lemak",
+            description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+            price: 10000.0
+        )
+        expect(food).to be_valid  
+    end
+    it "is invalid without a name" do
+        food = Food.new(
+            name: nil,
+            description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+            price: 10000.0
+        )
+        food.valid?
+        expect(food.errors[:name]).to include("can't be blank")
+    end
+    it "is invalid without a description" do
+        food = Food.new(
+            name: "Nasi Padang",
+            description: nil,
+            price: 20000.0
+        )
+        food.valid?
+        expect(food.errors[:description]).to include("can't be blank")  
+    end
+    it "is invalid with a duplicate name" do
+        food1 = Food.create(
+            name: "Nasi Uduk",
+            description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+            price: 10000.0
+        )
+
+        food2 = Food.new(
+            name: "Nasi Uduk",
+            description: "Just with a different decription",
+            price: 10000.0
+        )
+
+        food2.valid?
+        expect(food2.errors[:name]).to include("has already been taken")
+    end
+end
