8565c4efb87b6714ac8417c66dabaffde57da7b9
kevinhadinata <kevinhadinata86@gmail.com>
2018-08-14 12:33:21 +0000

    spec line item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180814115433) do
+ActiveRecord::Schema.define(version: 20180814122844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,4 +29,13 @@ ActiveRecord::Schema.define(version: 20180814115433) do
     t.datetime "updated_at",                          null: false
   end
 
+  create_table "line_items", force: :cascade do |t|
+    t.integer "food_id"
+    t.integer "cart_id"
+    t.index ["cart_id"], name: "index_line_items_on_cart_id", using: :btree
+    t.index ["food_id"], name: "index_line_items_on_food_id", using: :btree
+  end
+
+  add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "foods"
 end
